6894384c3b8d796d3a8a79e6cc69e1dcbf0ee4cc
kontelk <81286051+kontelk@users.noreply.github.com>
2021-04-11 12:49:13 +0300

    Update check-mx-records.sh

--- check-mx-records.sh
+++ check-mx-records.sh
@@ -8,20 +8,20 @@ declare -ar domains=(
 )
 declare -ar dns_servers=(
 	"192.168.1.1"
-	"1.1.1.1"
-	"4.4.4.4"
-	"8.8.8.8"
+	# "1.1.1.1"
+	# "4.4.4.4"
+	# "8.8.8.8"
 )
 declare -r timeout=2
 for i in "${domains[@]}"; do
 	for w in "${dns_servers[@]}"; do
 		result=$(nslookup -timeout="${timeout}" -query=mx "${i}" "${w}" 2>&1)
 		echo ""
-		echo "Searching MX Records for domain:	$i (using DNS:$w)"
+		echo "Searching MX Records for domain:  $i (using DNS:$w)"
 		echo ""
 		# echo $result; echo ""
 		if grep -qE '\*|;' <<< "${result}"; then
-			echo "	<<< MX RECORDS NOT FOUND! >>>	$(grep -E '\*|;' <<< "${result}")"
+			echo "	MX RECORDS NOT FOUND!	$(grep -E '\*|;' <<< "${result}")"
 			echo " "
 		elif grep -qi "mail exchanger =" <<< "${result}"; then
 			declare sub_result=$(grep -i "mail exchanger =" <<< "${result}" | awk '{print $6}')
@@ -30,9 +30,9 @@ for i in "${domains[@]}"; do
 			# echo "$sub_result_full"; echo ""
 			for exch in ${sub_result[@]}; do
 				exch="${exch::-1}"
-				echo "	Host: $exch"
+				echo "	MX record found (host):	$exch"
 				a_rec_exch=$(nslookup -timeout="${timeout}" -query=a "${exch}" "${w}" 2>&1)
-				echo "	$(   (grep -i 'Address:'  <<< "${a_rec_exch}") | tail -1 ) " 
+				echo "	$( (grep -i 'Address:' <<< "${a_rec_exch}") | tail -1 ) " 
 				echo ""
 			done
 		fi
